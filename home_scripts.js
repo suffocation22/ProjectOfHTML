76716756fdf497f4eb20ac8483850bceb252f789
suffocation22 <ronin633@gmail.com>
2022-08-13 16:19:03 +0300

    full project of HTML

---
+++ home_scripts.js
@@ -0,0 +1,14 @@
+function homePage() {
+    alert("Now you go to home page");
+}
+function NewMusic() {
+    alert("Now you go to new music from world, have fun:)");
+}
+
+function contantUs() {
+    alert("Now you go to Contant Us");
+}
+function changeColor() {
+    document.getElementById("myH1").style.color = "#004080";
+    document.getElementById("myH3").style.color = "#00b3b3";
+}
